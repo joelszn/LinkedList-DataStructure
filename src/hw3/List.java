511862638465d1ef8d275de0c5aeae24c439c6aa
Joel Duran <jduran3@albany.edu>
2017-05-12 19:51:16 -0400

    Part of Insert, Destroy, Size

--- src/hw3/List.java
+++ src/hw3/List.java
@@ -7,20 +7,66 @@ public class List {
 
 	// making the tail
 	private Node tail;
+	
 
-	//getters for head and tail since they're private *data encapsulation
+	// getters for head and tail since they're private *data encapsulation
 	public Node getHead() {
 		return this.head;
 	}
+
 	public Node getTail() {
 		return this.tail;
 	}
 
-	//setters
+	// setters
 	public void setHead(Node head) {
 		this.head = head;
 	}
+
 	public void setTail(Node tail) {
 		this.tail = tail;
 	}
+
+	// is Empty method evaluating head
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+	// Size method
+	public int size() {
+		Node temp= head;
+		int counter = 0;
+		
+		//while loop that iterates through nodes
+		while(temp !=null){
+			counter ++;
+			temp = temp.getNext();
+		}
+
+		return counter;
+
+	}
+
+	// Destroy
+	public void destroy(){
+		//Java destroys it since the head is null
+		//other programming languages don't exactly do this
+		head = null;
+	}
+
+	// Insert method
+	public void insert(String s) {
+		Node newNode = new Node(s);
+		if(isEmpty()){
+			//goes from the right assigning newNode to tail to head
+			head = tail = newNode;
+			
+		} else {
+			//only one thing in list
+			if (head == tail){
+				
+			}
+		}
+	}
+
 }
--- src/hw3/Node.java
+++ src/hw3/Node.java
@@ -22,20 +22,24 @@ public class Node {
 	public Node getPrev() {
 		return prev;
 	}
+
 	public Node getNext() {
 		return next;
 	}
+
 	public String getData() {
-		return data; 
+		return data;
 	}
 
 	// setters
 	public void setPrev(Node prev) {
 		this.prev = prev;
 	}
+
 	public void setNext(Node next) {
 		this.next = next;
 	}
+
 	public void setData(String data) {
 		this.data = data;
 	}
