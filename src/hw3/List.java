7e15dc874dce971ef6fc7adb22a493df079cd096
Joel Duran <jduran3@albany.edu>
2017-05-12 20:30:38 -0400

    Node Class Finished

--- src/hw3/List.java
+++ src/hw3/List.java
@@ -7,7 +7,6 @@ public class List {
 
 	// making the tail
 	private Node tail;
-	
 
 	// getters for head and tail since they're private *data encapsulation
 	public Node getHead() {
@@ -34,12 +33,12 @@ public class List {
 
 	// Size method
 	public int size() {
-		Node temp= head;
+		Node temp = head;
 		int counter = 0;
-		
-		//while loop that iterates through nodes
-		while(temp !=null){
-			counter ++;
+
+		// while loop that iterates through nodes
+		while (temp != null) {
+			counter++;
 			temp = temp.getNext();
 		}
 
@@ -48,22 +47,56 @@ public class List {
 	}
 
 	// Destroy
-	public void destroy(){
-		//Java destroys it since the head is null
-		//other programming languages don't exactly do this
+	public void destroy() {
+		// Java destroys it since the head is null
+		// other programming languages don't exactly do this
 		head = null;
 	}
 
 	// Insert method
 	public void insert(String s) {
 		Node newNode = new Node(s);
-		if(isEmpty()){
-			//goes from the right assigning newNode to tail to head
+		if (isEmpty()) {
+			// goes from the right assigning newNode to tail to head
 			head = tail = newNode;
-			
+
 		} else {
-			//only one thing in list
-			if (head == tail){
+			// only one thing in list
+			if (head == tail) {
+				// head.compareToIgnoreCase(newNode)
+
+				if (newNode.getData().compareToIgnoreCase(head.getData()) <= 0) {
+					// new head
+					head = newNode;
+					head.setNext(tail);
+					tail.setPrev(head);
+
+				} else {
+					// in the option if it comes afterwards
+					tail = newNode;
+					head.setNext(tail);
+					tail.setPrev(head);
+
+				}
+			} else {
+				// if it comes before the head
+				if(){
+					// if it comes after the tail
+				} else if(){
+					
+					//if it's here it comes somewhere in between head and tail
+				} else{
+					// loop to traverse through list
+					while(){
+						// if it goes AFTER the while loop's node and BEFORE while loop's node.getNext, insert here
+						if(() && ()){
+							
+							// otherwise, set the while loop's node to it's next and keep looking
+						} else {
+							
+						}
+					}
+				}
 				
 			}
 		}
--- src/hw3/Main.java
+++ src/hw3/Main.java
@@ -1,11 +1,9 @@
 package hw3;
 
 public class Main {
-	
-	public static void main(String[] args){
-		
-		
-		
+
+	public static void main(String[] args) {
+
 	}
 
 }
